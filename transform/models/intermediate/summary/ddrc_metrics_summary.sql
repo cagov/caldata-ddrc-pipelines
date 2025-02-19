4db7e65a546ce0caeecb595b225ead1ec8996556
summer-mothwood <summer.mothwood@innovation.ca.gov>
2025-02-18 16:29:17 -0800

    Update ddrc_metrics_summary.sql
    fixing linting issues

--- transform/models/intermediate/summary/ddrc_metrics_summary.sql
+++ transform/models/intermediate/summary/ddrc_metrics_summary.sql
@@ -7,19 +7,19 @@
 with epa_count_phase1_complete as (
     select
         'cleanup_phase1_complete' as metric_name,
-        sum(PUBLIC_STATUS_COUNT) as metric_value,
+        sum(public_status_count) as metric_value,
         last_updated
     from {{ ref('public_status_by_land_use_and_fire_name_counted') }}
-    where PUBLIC_STATUS = 'Phase 1 Complete'
-    and LAND_USE = 'Residential'
-    group by PUBLIC_STATUS, LAST_UPDATED
-), 
-
+    where
+        public_status = 'Phase 1 Complete'
+        and land_use = 'Residential'
+    group by public_status, last_updated
+),
 -- As additional metrics are added to the DDRC, union them here:
 ddrc_metrics as (
     select * from epa_count_phase1_complete
     -- union all
-    --select * from acoe_metric 
+    --select * from acoe_metric
 )
 
 select * from ddrc_metrics

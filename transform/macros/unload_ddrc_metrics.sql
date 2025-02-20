e19edc4afd2a14ed7173ae429e4965f3a2e2eaa8
Ian Rose <ian.rose@innovation.ca.gov>
2025-02-20 12:51:49 -0800

    Add macro to unload ddrc metrics

---
+++ transform/macros/unload_ddrc_metrics.sql
@@ -0,0 +1,16 @@
+{% macro unload_ddrc_metrics() -%}
+  {% if target.name == 'prd' %}
+    {% set stage = '@ANALYTICS_DDRC_DEV.PUBLIC.MARTS' %}
+  {% else %}
+    {% set stage = '@ANALYTICS_DDRC_DEV.PUBLIC.MARTS' %}
+  {% endif %}
+  {% set key = 'ddrc-metrics.json' %}
+  {% set url = stage ~ '/' ~ key %}
+      copy into {{ url }}
+      from (
+        select array_agg(object_construct(*)) from {{ this }}
+      )
+      file_format = (type=json compression=none)
+      single = true
+      overwrite = true;
+{% endmacro %}
